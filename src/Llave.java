456f01e81dff68d1a4597dba3f3cf3f5809a2689
escuela@davinci.edu.ar <gianni.baldizzone@davinci.edu.ar>
2022-11-28 12:55:10 -0300

    seguimiento

--- src/Llave.java
+++ src/Llave.java
@@ -4,7 +4,22 @@ public class Llave extends EtapaMundial{
     public Llave(String descripcionEtapa, ArrayList<Partido> partidos) {
         super(descripcionEtapa, partidos);
     }
+    public void armarLlaveFinal(Equipo Equipo1,Equipo Equipo2){
 
+
+    }
+    public void armarLlaveSemi(Equipo Equipo1,Equipo Equipo2,Equipo Equipo3,Equipo Equipo4){
+
+
+    }
+    public void armarLlaveCuartos(Equipo Equipo1,Equipo Equipo2){
+
+
+    }
+    public void armarLlaveOctavos(Equipo Equipo1,Equipo Equipo2){
+
+
+    }
     @Override
     public ArrayList<Equipo> getEquiposQueAvanzan() {
         return super.getEquiposQueAvanzan();
